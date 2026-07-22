82ad6416851dc2e7b198582728505720d78500f6
Carmelito Andrade <carmelito0604@gmail.com>
2026-07-22 16:00:09 +0530

    Using Servo and Keypad - switch case

---
+++ Servo_Keypad_Stanley.ino
@@ -0,0 +1,53 @@
+
+#include <Keypad.h>
+#include <Servo.h>
+
+Servo myServo;
+int position=90;
+int value;
+
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3', 'A'},
+  {'4','5','6', 'B'},
+  {'7','8','9', 'C'},
+  {'*','0','#', 'D'}
+};
+byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+  myServo.attach(10);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key!=0){
+    switch(key){
+      case '1':
+      position = 0;
+      break;
+
+      case '2':
+      position = 45;
+      break;
+
+      case '3':
+      position = 90;
+      break;
+
+      default:
+      Serial.println("Invalid Key Pressed");
+      break;
+
+    }
+    myServo.write(position);
+  }
+  delay(10);
+}
