12449b7189b9c9ee73a7eb5d34909b84fa650e5b
Carmelito Andrade <carmelito0604@gmail.com>
2026-07-31 16:55:56 +0530

    Snickometer Uploading sound sensor value to thingspeak

---
+++ ProjectsAITD/Cricket_Snikometer/SnickoMeterThingspeak_1.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
+
+const int analogInPin = A0;  // Gas sensor connected to A0 on ESP8266 NodeMCU
+int sensorValue = 0;  // value read from the pot
+
+// Replace with your network credentials
+char ssid[] = "AITD";         
+char pass[] = "123456789";   
+
+WiFiClient client;
+
+// Replace with your ThingSpeak Channel details
+unsigned long myChannelNumber = 3441361; 
+const char * myWriteAPIKey = "BBX3U5S3X8K0NVLL";  
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);   
+  ThingSpeak.begin(client);  
+}
+
+void loop() {
+  // Connect or reconnect to Wi-Fi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass); 
+      delay(5000);     
+    }
+    Serial.println("\nConnected.");
+  }
+
+  //Reading gas sensor values
+  sensorValue = analogRead(analogInPin);
+  // Write value to Field 1 of your ThingSpeak Channel
+  int x = ThingSpeak.writeField(myChannelNumber, 1, sensorValue, myWriteAPIKey);
+  
+  if (x == 200) {
+    Serial.println("Channel update successful.");
+  } else {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+
+  // ThingSpeak free tier requires at least 15 seconds between updates
+  delay(20000); 
+}
